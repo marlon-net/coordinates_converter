5fbc8480b94e4294c5bc25dda7811f6fee7a626d
Marlon Dias <marlon@marlon.net>
2016-08-10 19:39:37 -0300

    Update Coordinates.java

--- Coordinates.java
+++ Coordinates.java
@@ -1,4 +1,6 @@
-import java.util.ArrayList;
+/*
+    Convert cartesian (x,v)  to Polar (ray, angle)
+*/
 
 public class Coordinates {
 
@@ -18,6 +20,7 @@ public class Coordinates {
         mRay = Math.hypot(mX,mY);
         mAngleRadians = (mRay!=0) ? Math.acos(mX / mRay) : 0.0;
         if ( mY < 0 ) {
+            // angle need be corrected to 4rd quadrant
             mAngleRadians = ( Math.PI * 2 ) - mAngleRadians;
         }
     }
@@ -37,69 +40,13 @@ public class Coordinates {
 
     ///////  "getters"
     public double getX() {  return mX;  }
-    public String getX(int decimals) {
-        return String.format("%." + decimals + "f", mX);
-    }
 
     public double getY() {  return mY;  }
-    public String getY(int decimals) {
-        return String.format("%." + decimals + "f", mY);
-    }
 
     public double getRay() { return mRay; }
-    public String getRay(int decimals) {
-        return String.format("%." + decimals + "f", mRay);
-    }
 
     public double getAngleRadians() { return mAngleRadians; }
-    public String getAngleRadians(int decimals) {
-        return comum.formatDouble( mAngleRadians, decimals );
-    }
 
     public double getAngleDegrees() { return Math.toDegrees(mAngleRadians); }
-    public String getAngleDegrees(int decimals) {
-        return comum.formatDouble( this.getAngleDegrees(), decimals );
-    }
 
-
-    ////// get arrays
-
-    public Double[] getArrayOfNumbers(Coordinates coordinates) {
-
-        // empty array to be filled and returned
-        Double[] array = new Double[0];
-
-        ArrayList<Double> arrayList = new ArrayList<Double>();
-        arrayList.add(coordinates.getX());
-        arrayList.add(coordinates.getY());
-        arrayList.add(coordinates.getRay());
-        arrayList.add(coordinates.getAngleRadians());
-        arrayList.add(coordinates.getAngleDegrees());
-
-        array = arrayList.toArray(array);
-        return array;
-    }
-
-    public Double[] getArrayOfNumbers() {
-        return getArrayOfNumbers(this);
-    }
-
-    public String[] getArrayOfStrings(Coordinates coordinates, int decimals) {
-
-        // empty array of strings to be filled and returned
-        String[] array = new String[0];
-
-        ArrayList<String> arrayList = new ArrayList<String>();
-        arrayList.add(coordinates.getX(decimals));            
-        arrayList.add(coordinates.getY(decimals));            
-        arrayList.add(coordinates.getRay(decimals));          
-        arrayList.add(coordinates.getAngleRadians(decimals)); 
-        arrayList.add(coordinates.getAngleDegrees(decimals)); 
-
-        array = arrayList.toArray(array);
-        return array;
-    }
-
-    public String[] getArrayOfStrings(int decimals) {
-        return getArrayOfStrings(this, decimals);
-    }
+}
